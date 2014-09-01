0159dcd6530f7c59daf04e26f3b06b2a2c5e3287
HyperGamers <HyperGamers@users.noreply.github.com>
2014-09-01 11:03:12 +0100

    Update Main.php

--- src/JoinNotification/Main.php
+++ src/JoinNotification/Main.php
@@ -7,43 +7,40 @@ use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
 
-class Main extends PluginBase implements Listener{
-   
+class Main extends PluginBase implements Listener{                                       
     public function onEnable() {
-       
+
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        
+
         $this->getLogger()->info("Everything loaded!");
     }
-       
-       
+
+
     }
-   
+
     /**
     * @param PlayerJoinEvent $event
     *
     * @priority HIGHEST
     * @ignoreCancelled true
     */
-    function onJoin(PlayerJoinEvent $event) {
-       
+    public function onJoin(PlayerJoinEvent $event) {
+
         $player = $event->getPlayer();
         $name = $player->getDisplayName();
-       
-        if  ($player->isOp()) {
-           
-            $this->getServer()->broadcastMessage(TextFormat::DARK_RED . "Hello! Guys, " .$name." [ADMIN] joined the game!");
-           
-        }
-       
-        else {
-                    $this->getServer()->broadcastMessage(TextFormat::DARK_GREEN . "Hello! Guys, ".$name." [DEFAULT] joined the game!");
 
-       
+        if ($player->isOp()) {
+
+            $this->getServer()->broadcastMessage("Hello! Guys,".$name." [ADMIN] joined the game!");
+        }
+
+        else {
+                    $this->getServer()->broadcastMessage("Hello! Guys,".$name." [DEFAULT] joined the game!");
+
+
     }
-   
+
 }
 
 ?>
